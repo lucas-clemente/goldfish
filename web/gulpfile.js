bf7e1ad2cc7ce955654cf7d789d38f2e8fc21bb7
Lucas Clemente <lucas@clemente.io>
2014-10-17 23:36:49 +0200

    backbone web app

---
+++ web/gulpfile.js
@@ -0,0 +1,81 @@
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+
+
+var config = {
+  js: {
+    src: 'js/*.js',
+    dest: 'dist/assets',
+  },
+
+  css: {
+    src: 'style/*.scss',
+    dest: 'dist/assets',
+  },
+
+  html: {
+    src: 'index.html',
+    dest: 'dist',
+  },
+
+  vendor_js: {
+    src: [
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/underscore/underscore.js',
+      'bower_components/backbone/backbone.js',
+    ],
+    dest: 'dist/assets',
+  },
+};
+
+gulp.task('js', function () {
+  return gulp.src(config.js.src)
+    .pipe(concat({path: 'app.js'}))
+    .pipe(gulp.dest(config.js.dest))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  return gulp.src(config.css.src)
+    .pipe(concat({path: 'app.css'}))
+    .pipe(sass())
+    .pipe(gulp.dest(config.css.dest))
+    .pipe(connect.reload());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(config.js.src)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('html', function () {
+  return gulp.src(config.html.src)
+    .pipe(gulp.dest(config.html.dest))
+    .pipe(connect.reload());
+});
+
+gulp.task('vendor-js', function () {
+  return gulp.src(config.vendor_js.src)
+    .pipe(concat({path: 'vendor.js'}))
+    .pipe(gulp.dest(config.vendor_js.dest));
+});
+
+gulp.task('server', function() {
+  connect.server({
+    root: 'dist',
+    fallback: 'index.html',
+    livereload: true,
+  });
+});
+
+gulp.task('watch', ['server'], function () {
+  gulp.watch([config.js.src, config.css.src, config.html.src], ['html', 'js', 'css']);
+});
+
+
+gulp.task('default', ['lint', 'js', 'css', 'html', 'vendor-js']);
---
+++ web/js/app.js
@@ -0,0 +1,125 @@
+var Page = Backbone.Model.extend({
+  defaults: {
+    html: "",
+    loaded: false,
+  }
+});
+
+var PageItemView = Backbone.View.extend({
+  tagName: 'a',
+  className: 'list-group-item',
+
+  render: function () {
+    this.$el.text(this.model.id);
+    this.$el.attr('href', this.model.id)
+    return this;
+  },
+});
+
+var PageList = Backbone.Collection.extend({
+  model: Page
+});
+
+var PageListView = Backbone.View.extend({
+  tagName: "ul",
+  className: 'list-group',
+
+  initialize: function () {
+    this.collection.on("reset", this.render, this);
+  },
+
+  render: function () {
+    this.addAll();
+  },
+
+  addOne: function (page) {
+    var itemView = new PageItemView({model: page});
+    this.$el.append(itemView.render().el);
+  },
+
+  addAll: function () {
+    this.collection.forEach(this.addOne, this);
+  }
+});
+
+var PageView = Backbone.View.extend({
+  render: function () {
+    this.$el.html(this.model.get("html"));
+  },
+
+  setModel: function (model) {
+    this.model = model;
+    if (!this.model.get("loaded")) {
+      this.model.set("html", "<h1>" + this.model.get("id") + "</h1>");
+      this.model.set("loaded", true);
+      this.render();
+    } else {
+      this.render();
+    }
+  }
+});
+
+
+var App = Backbone.Router.extend({
+  routes: {
+    "":              "folder",
+    "*folder/":      "folder",
+    "*folder/:page": "page",
+    ":page":         "page",
+  },
+
+  folder: function (folder) {
+    console.log("folder: ", folder);
+  },
+
+  page: function (folder, page) {
+    if (!page) {
+      page = folder;
+      folder = '';
+    }
+    var model = window.appView.pageList.get(folder + '/' + page);
+    window.appView.pageView.setModel(model);
+  },
+});
+
+
+var AppView = Backbone.View.extend({
+  initialize: function () {
+    this.pageView = new PageView({model: new Page({id: "/"})});
+    $('#page').append(this.pageView.el);
+    this.pageList = new PageList();
+    this.pageListView = new PageListView({collection: this.pageList});
+    $('#list').append(this.pageListView.el);
+    this.fetchCollections();
+  },
+
+  fetchCollections: function () {
+    var items = [
+      {id: "/foo", text: "item one" },
+      {id: "/bar", text: "item two" },
+      {id: "/baz", text: "item three" }
+    ];
+    this.pageList.reset(items);
+  }
+});
+
+// From https://gist.github.com/tbranyen/1142129
+$(document).delegate("a", "click", function(evt) {
+  // Get the anchor href and protcol
+  var href = $(this).attr("href");
+  var protocol = this.protocol + "//";
+
+  // Ensure the protocol is not part of URL, meaning its relative.
+  // Stop the event bubbling to ensure the link will not cause a page refresh.
+  if (href.slice(protocol.length) !== protocol) {
+    evt.preventDefault();
+    window.app.navigate(href, {trigger: true});
+  }
+});
+
+
+$(function () {
+  window.app = new App();
+  window.appView = new AppView();
+  Backbone.history.start({pushState: true});
+});
