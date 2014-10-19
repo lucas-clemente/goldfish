fcf533c94214bff0f4b9023fa090b47df893ad8c
Lucas Clemente <lucas@clemente.io>
2014-10-19 21:18:32 +0200

    button for parent folder

--- web/js/views/folder_list_view.js
+++ web/js/views/folder_list_view.js
@@ -4,6 +4,7 @@ import FolderItemView from 'folder_item_view';
 export default Backbone.View.extend({
   tagName: "ul",
   className: 'list-group',
+  folderUpTemplate: _.template($("#template-folder-up").html()),
 
   initialize: function () {
     this.model.on("change", this.render, this);
@@ -11,6 +12,10 @@ export default Backbone.View.extend({
 
   render: function () {
     this.$el.empty();
+    if (this.model.id != "/") {
+      var upURL = this.model.id.slice(0, this.model.id.slice(0, -1).lastIndexOf("/")+1);
+      this.$el.append($(this.folderUpTemplate({url: upURL})));
+    }
     this.model.attributes.subFolders.forEach(function (f) {
       this.$el.append((new FolderItemView({model: f})).render().el);
     }, this);
