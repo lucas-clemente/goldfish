703fa278f94982e3f5ea86a4d0e7bc32372972a8
Lucas Clemente <lucas@clemente.io>
2015-05-03 23:17:20 +0200

    initial ember project

---
+++ goldfish/Brocfile.js
@@ -0,0 +1,33 @@
+/* global require, module */
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+var app = new EmberApp();
+
+// Use `app.import` to add additional libraries to the generated
+// output files.
+//
+// If you need to use different assets in different
+// environments, specify an object as the first parameter. That
+// object's keys should be the environment name and the values
+// should be the asset to use in that environment.
+//
+// If the library that you are including contains AMD or ES6
+// modules that you would like to import into your application
+// please specify an object with the list of modules as keys
+// along with the exports of each module as its value.
+
+app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
+
+var faDir = {
+  destDir: 'fonts'
+};
+
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff', faDir);
+
+app.import('bower_components/highlightjs/highlight.pack.js');
+app.import('bower_components/highlightjs/styles/tomorrow.css');
+
+app.import('bower_components/marked/lib/marked.js');
+
+module.exports = app.toTree();
