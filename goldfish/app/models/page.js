fe632d13a207c62df78e00b111b930d9302cff3e
Lucas Clemente <lucas@clemente.io>
2015-05-04 22:43:02 +0200

    fix remote links

--- goldfish/app/models/page.js
+++ goldfish/app/models/page.js
@@ -21,15 +21,22 @@ export default DS.Model.extend({
     this._super.apply(this, arguments);
     this.markdownRenderer = new marked.Renderer();
 
-    this.markdownRenderer.image = (href, title, text) => {
+    this.markdownRenderer.image = (href, title) => {
       if (href[0] === '/') {
         href = '/v1' + href;
       } else {
-        href = '/v1/' + this.id.slice(0, this.id.lastIndexOf('/')+1) + href;
+        href = '/v1' + this.currentFolder() + href;
       }
       return '<div class="image"><img src="' + href + '" title="' + (title || '') + '" class="img-thumbnail" /></div>';
     };
 
+    // Make all links absolute
+    this.markdownRenderer.link = (href, title, text) => {
+      if (href.search(/http/) !== 0 && href[0] !== '/') {
+        href = this.currentFolder() + href;
+      }
+      return '<a href="' + href + '">' + text + '</a>';
+    };
   },
 
   compiled: Ember.computed('text', function () {
@@ -55,4 +62,8 @@ export default DS.Model.extend({
 
     return compiled;
   }),
+
+  currentFolder: function () {
+    return "/" + this.id.slice(0, this.id.lastIndexOf('/')+1);
+  },
 });
