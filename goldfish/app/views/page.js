a8c5c386204515fe2b9cd4510682417b27d22d21
Lucas Clemente <lucas@clemente.io>
2015-05-04 23:39:31 +0200

    fix internal links causing page reload

--- goldfish/app/views/page.js
+++ goldfish/app/views/page.js
@@ -2,19 +2,34 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   didInsertElement: function () {
-    this.runTex();
+    this.updateDomStuff();
   },
 
-  runTex: Ember.observer('controller.model.compiled', function () {
-    this.$('script').each(function (i, e) {
-      var t = e.getAttribute('type');
-      if (t.search('math/tex') === 0) {
-        katex.render(e.textContent,
-                     e.parentElement,
-                     {
-                       displayMode: t.search('mode=display') !== -1
-                     });
-      }
+  updateDomStuff: Ember.observer('controller.model.compiled', function () {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      // Render tex
+      this.$('script').each(function (i, e) {
+        var t = e.getAttribute('type');
+        if (t.search('math/tex') === 0) {
+          katex.render(e.textContent,
+                       e.parentElement,
+                       {
+                         displayMode: t.search('mode=display') !== -1
+                       });
+        }
+      });
+
+      // Fix internal links
+      this.$('a').each((i, el) => {
+        var href = el.getAttribute('href');
+        if (href[0] === '/') {
+          Ember.$(el).click((ev) => {
+            var router = this.get('controller.target.router');
+            router.transitionTo(href);
+            ev.preventDefault();
+          });
+        }
+      });
     });
   }),
 });
