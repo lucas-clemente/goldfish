aaa2947a13d905d996af71c577d380be7d714e72
Lucas Clemente <lucas@clemente.io>
2015-05-13 21:24:19 +0200

    set window title properly, fixes #34

--- goldfish/app/routes/folder.js
+++ goldfish/app/routes/folder.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  titleToken: function (folder) {
+    return folder.get('path');
+  },
+
   model: function (params) {
     return this.store.find('folder', params.folder_id);
   },
--- goldfish/app/routes/page.js
+++ goldfish/app/routes/page.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  titleToken: function (page) {
+    return page.get('name');
+  },
+
   model: function (params) {
     return this.store.find('page', params.page_id);
   }
