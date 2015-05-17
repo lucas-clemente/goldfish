a4226338d8a7c46736ea8f52781556c52c3501ba
Lucas Clemente <lucas@clemente.io>
2015-05-17 17:54:38 +0200

    remove unneccessary buildURL in app adapter

--- web/app/adapters/application.js
+++ web/app/adapters/application.js
@@ -2,8 +2,4 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: 'v2',
-
-  buildURL: function (type, id) {
-    return this.namespace + '/' + type + 's/' + id;
-  },
 });
