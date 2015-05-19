218e4874e2cc6e9e494a2e61fd01e3f176e89d02
Lucas Clemente <lucas@clemente.io>
2015-05-19 11:28:22 +0200

    display modification date for pages

---
+++ web/tests/unit/helpers/moment-calendar-test.js
@@ -0,0 +1,10 @@
+import { momentCalendar } from '../../../helpers/moment-calendar';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | moment calendar');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = momentCalendar(42);
+  assert.ok(result);
+});
