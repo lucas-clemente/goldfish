7d797eb518fed1b8c0d0dfea9ff8cac71cf4c527
Lucas Clemente <lucas@clemente.io>
2015-05-19 11:07:41 +0200

    update ember-cli to 0.2.5

--- web/app/router.js
+++ web/app/router.js
@@ -2,13 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType,
+  location: config.locationType
 });
 
-export default Router.map(function() {
+Router.map(function() {
   this.resource('folder', {path: '/folders/:folder_id'}, function () {
     this.resource('page', {path: '/pages/:page_id'});
   });
   this.route('not-found', {path: '/*path'});
   this.route('pages', {path: '/pages'});
 });
+
+export default Router;
