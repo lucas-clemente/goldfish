a0a68b631d37018b25400b851143f62e36c95354
Lucas Clemente <lucas@clemente.io>
2015-05-28 15:48:04 +0200

    fix ordering of newly added files

--- web/app/models/folder.js
+++ web/app/models/folder.js
@@ -6,6 +6,15 @@ export default DS.Model.extend({
   subfolders: DS.hasMany('folder', {async: true, inverse: 'parentFolder'}),
   parentFolder: DS.belongsTo('folder', {async: true}),
 
+  sortedPages: Ember.computed.sort('pages', function (a, b) {
+    if (a.id < b.id) {
+      return -1;
+    } else if (a.id > b.id) {
+      return 1;
+    }
+    return 0;
+  }),
+
   name: Ember.computed('id', function () {
     var name = this.id.slice(this.id.lastIndexOf('|') + 1);
     if (name === "") {
